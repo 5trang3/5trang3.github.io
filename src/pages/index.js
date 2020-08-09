007503e444934eb363d984d3eb4a9af798d1081c
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-08-08 20:53:43 -0700

    Add heading and image to main page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
 import React from "react"
 import Header from '../components/header.js'
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import 'fontsource-roboto';
+import Portrait from '../images/portrait.jpg'
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'grid',
+    gridTemplateColumns: '50% 50%',
+    justifyItems: 'center'
+  },
+}))
 export default function Home() {
+  const classes = useStyles();
   return (
     <div>
       <Header/>
+      <Container className={ classes.container }>
+          <Typography variant='h1' gutterBottom>Hi, My name is Prasant Prasath, and I am a Full stack developer</Typography>
+          <img src={ Portrait } alt='A picture of Prasant' id='portrait'></img>
+      </Container>
     </div>
   )
 }
