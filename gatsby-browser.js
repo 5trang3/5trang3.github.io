bca542f8cf326173f3e9d56237ba90c4637923f5
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-08-14 15:49:31 -0700

    Install Caveat font.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,4 @@ import 'fontsource-roboto/300.css';
 import 'fontsource-roboto/400.css';
 import 'fontsource-roboto/500.css';
 import 'fontsource-roboto/700.css';
+import 'fontsource-caveat/400.css';
