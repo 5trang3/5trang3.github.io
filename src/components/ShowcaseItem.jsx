a3425a2a5989d41f5d1593b5b973da620f4479f3
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-09-13 00:03:11 -0700

    Fix linter errors in ShowcaseItem component.

---
+++ src/components/ShowcaseItem.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+import PropTypes from 'prop-types';
+import '../styles/components/showcaseItem.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from './CustomButton';
+
+const useStyles = makeStyles(() => ({
+  card: {
+    height: '100%',
+    display: 'grid',
+    gridTemplateColumns: '80% 20%',
+    gridTemplateRows: '20% 55% 25%',
+  },
+  cardHeader: {
+    gridColumn: '1 / 2',
+  },
+  cardActions: {
+    gridColumn: '2 / 3',
+  },
+  cardMedia: {
+    gridColumn: '1 / 3',
+  },
+  cardContent: {
+    gridColumn: '1 / 3',
+    display: 'grid',
+    gridTemplateRows: '66% 33%',
+    rowGap: '10px',
+  },
+  chip: {
+    marginRight: '5px',
+  },
+}));
+
+const ShowcaseItem = ({
+  chips, link, title, githubLink, img, content,
+}) => {
+  const classes = useStyles();
+  const chipList = chips.map((chip) => <Chip label={chip} size="small" className={classes.chip} />);
+
+  return (
+    <div>
+      <a href={link} className="showcase-link">
+        <Card className={classes.card}>
+          <CardHeader title={title} className={classes.cardHeader} />
+          <CardActions className={classes.cardActions}>
+            <a href={githubLink}>
+              <Button buttonName="github" buttonColor="primary" textColor="#00578e" />
+            </a>
+          </CardActions>
+          <CardMedia image={img} className={classes.cardMedia} />
+          <CardContent className={classes.cardContent}>
+            <Typography className={classes.description} variant="body2">
+              {content}
+            </Typography>
+            <div className="chip-container">{chipList}</div>
+          </CardContent>
+        </Card>
+      </a>
+    </div>
+  );
+};
+
+ShowcaseItem.propTypes = {
+  chips: PropTypes.arrayOf(PropTypes.string).isRequired,
+  link: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  githubLink: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+export default ShowcaseItem;
