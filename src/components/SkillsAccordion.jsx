8acff8803b84cc0c2bd40acdf720f7281870188c
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-10-09 15:31:00 -0700

    Give Accordion 100% width at small screen sizes and add a bottom margin.

--- src/components/SkillsAccordion.jsx
+++ src/components/SkillsAccordion.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import SkillsAccordionItem from './SkillsAccordionItem';
 
-const SkillsDisplay = ({ skills }) => (
-  <div style={{ width: '50%' }}>
-    {skills.map((skill) => (
-      <React.Fragment key={`${skill.icon}-accordion-item`}>
-        <SkillsAccordionItem skill={skill} />
-      </React.Fragment>
-    ))}
-  </div>
-);
+const SkillsDisplay = ({ skills }) => {
+  const isSmallWidth = useMediaQuery((theme) => theme.breakpoints.down('xs'));
+  const accordionStyles = { marginBottom: '30px' };
+  accordionStyles.width = isSmallWidth ? '100%' : '50%';
+  return (
+    <div style={accordionStyles}>
+      {skills.map((skill) => (
+        <React.Fragment key={`${skill.icon}-accordion-item`}>
+          <SkillsAccordionItem skill={skill} />
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
 
 SkillsDisplay.propTypes = {
   skills: PropTypes.arrayOf(
