2ed4f3719e80d72f64eb5bc99346d7d9001a449d
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-10-11 00:36:45 -0700

    Add cover transition between pages.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Link } from 'gatsby';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
 import CustomButton from './CustomButton';
 
 const useStyles = makeStyles(() => ({
@@ -14,23 +14,25 @@ const useStyles = makeStyles(() => ({
 
 export default () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const secondaryColor = theme.palette.secondary.main;
   return (
     <div>
       <AppBar>
         <Toolbar className={classes.toolbar}>
-          <Link to="/">
+          <AniLink to="/" cover bg={secondaryColor}>
             <CustomButton buttonName="home" />
-          </Link>
+          </AniLink>
           <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr' }}>
-            <Link to="/about/">
+            <AniLink to="/about/" cover direction="right">
               <CustomButton buttonName="about" />
-            </Link>
-            <Link to="/skills/">
+            </AniLink>
+            <AniLink to="/skills/" cover direction="right">
               <CustomButton buttonName="skills" />
-            </Link>
-            <Link to="/showcase/">
+            </AniLink>
+            <AniLink to="/showcase/" cover direction="right">
               <CustomButton buttonName="showcase" />
-            </Link>
+            </AniLink>
           </div>
         </Toolbar>
       </AppBar>
